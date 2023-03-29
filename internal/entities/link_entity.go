599ffd30adcb1b010fe67f5f8dbe03b1af653a2e
Pedro <p-cardozo@hotmail.com>
2023-03-29 07:25:10 -0300

    feat(entity): include user & link entities

---
+++ internal/entities/link_entity.go
@@ -0,0 +1,3 @@
+package entities
+
+type LinkEntity struct{}
---
+++ internal/entities/user_entity.go
@@ -0,0 +1,16 @@
+package entities
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	return nil, nil
+}
